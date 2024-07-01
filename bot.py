4fb4b2adbd9665624dbd6da013e14c3071a8de9e
Smart Airdrop <smileuptn@gmail.com>
2024-07-01 19:15:14 +0700

    Fix login and claim

--- bot.py
+++ bot.py
@@ -111,6 +111,8 @@ class MatchQuest:
         headers["authorization"] = token
         payload = json.dumps({"uid": user_id})
 
+        headers["Content-Length"] = str(len(payload))
+
         response = requests.post(url=url, headers=headers, data=payload)
 
         return response
@@ -120,6 +122,8 @@ class MatchQuest:
         headers = self.headers()
         headers["authorization"] = token
         payload = json.dumps({"uid": user_id})
+
+        headers["Content-Length"] = str(len(payload))
 
         response = requests.post(url=url, headers=headers, data=payload)
 
@@ -178,14 +182,14 @@ class MatchQuest:
         print(f"{black}[{now}]{reset} {msg}{reset}")
 
     def main(self):
-        self.clear_terminal()
-        print(self.banner)
-        data = open(data_file, "r").read().splitlines()
-        num_acc = len(data)
-        self.log(self.line)
-        self.log(f"{green}Numer of account: {white}{num_acc}")
         while True:
+            self.clear_terminal()
+            print(self.banner)
             end_at_list = []
+            data = open(data_file, "r", encoding="utf8").read().strip().split("\n")
+            num_acc = len(data)
+            self.log(self.line)
+            self.log(f"{green}Numer of account: {white}{num_acc}")
             for no, data in enumerate(data):
                 self.log(self.line)
                 self.log(f"{green}Account number: {white}{no+1}/{num_acc}")
@@ -214,22 +218,6 @@ class MatchQuest:
                     except Exception as e:
                         self.log(f"{red}Get balance error!!!")
 
-                    # Reward
-                    try:
-                        get_reward = self.get_reward(
-                            token=token, user_id=user_id
-                        ).json()
-                        end_at = (
-                            float(get_reward["data"]["next_claim_timestamp"]) / 1000
-                        )
-                        readable_time = datetime.fromtimestamp(end_at).strftime(
-                            "%Y-%m-%d %H:%M:%S"
-                        )
-                        self.log(f"{green}Farm end at: {white}{readable_time}")
-                        end_at_list.append(end_at)
-                    except Exception as e:
-                        self.log(f"{red}Get reward error!!!")
-
                     # Claim from ref
                     try:
                         self.log(f"{yellow}Trying to claim from ref...")
@@ -246,40 +234,6 @@ class MatchQuest:
                     except Exception as e:
                         self.log(f"{red}Claim from ref error!!!")
 
-                    # Claim
-                    try:
-                        self.log(f"{yellow}Trying to claim...")
-                        claim = self.claim(token=token, user_id=user_id)
-
-                        if claim.status_code == 200:
-                            self.log(f"{green}Claim successful!")
-                            # Balance
-                            try:
-                                get_balance = self.get_balance(
-                                    token=token, user_id=user_id
-                                ).json()
-                                balance = get_balance["data"]
-                                self.log(
-                                    f"{green}Balance after Claim: {white}{balance / 1000}"
-                                )
-                            except Exception as e:
-                                self.log(f"{red}Get balance error!!!")
-
-                            # Farming
-                            try:
-                                self.log(f"{yellow}Trying to farm...")
-                                farming = self.farming(token=token, user_id=user_id)
-                                if farming.status_code == 200:
-                                    self.log(f"{green}Farm successful!")
-                                else:
-                                    self.log(f"{yellow}Not time to farm yet!")
-                            except Exception as e:
-                                self.log(f"{red}Farming error!!!")
-                        else:
-                            self.log(f"{yellow}Not time to claim yet!")
-                    except Exception as e:
-                        self.log(f"{red}Claim error!!!")
-
                     # Play game
                     if self.autogame:
                         self.play_game(token=token)
@@ -293,6 +247,61 @@ class MatchQuest:
                         except Exception as e:
                             self.log(f"{red}Get balance error!!!")
 
+                    # Reward and Farming/Claim
+                    try:
+                        while True:
+                            get_reward = self.get_reward(
+                                token=token, user_id=user_id
+                            ).json()
+                            next_claim = get_reward["data"]["next_claim_timestamp"]
+                            if next_claim == 0:
+                                self.log(f"{yellow}Trying to farm...")
+                                farming = self.farming(token=token, user_id=user_id)
+                                if farming.status_code != 200:
+                                    self.log(f"{red}Cannot process farming!")
+                                    continue
+                                else:
+                                    self.log(f"{green}Farm successful!")
+                            if next_claim > round(time.time() * 1000):
+                                self.log(f"{yellow}Not time to claim yet!")
+                                end_at = (
+                                    float(get_reward["data"]["next_claim_timestamp"])
+                                    / 1000
+                                )
+                                readable_time = datetime.fromtimestamp(end_at).strftime(
+                                    "%Y-%m-%d %H:%M:%S"
+                                )
+                                self.log(f"{green}Farm end at: {white}{readable_time}")
+                                end_at_list.append(end_at)
+                                break
+                            self.log(f"{yellow}Trying to claim...")
+                            claim = self.claim(token=token, user_id=user_id)
+                            if claim.status_code != 200:
+                                self.log(f"{red}Cannot claim right now!")
+                                break
+                            else:
+                                self.log(f"{green}Claim successful!")
+                                self.log(f"{yellow}Trying to farm...")
+                                farming = self.farming(token=token, user_id=user_id)
+                                if farming.status_code != 200:
+                                    self.log(f"{red}Cannot process farming!")
+                                    continue
+                                else:
+                                    self.log(f"{green}Farm successful!")
+
+                            # Balance
+                            try:
+                                get_balance = self.get_balance(
+                                    token=token, user_id=user_id
+                                ).json()
+                                balance = get_balance["data"]
+                                self.log(
+                                    f"{green}Current balance: {white}{balance / 1000}"
+                                )
+                            except Exception as e:
+                                self.log(f"{red}Get balance error!!!")
+                    except Exception as e:
+                        self.log(f"{red}Get reward error!!!")
                 except Exception as e:
                     self.log(f"{red}Login error!!!")
                     print(2)
